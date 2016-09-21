87f7eaa77fd6492597cbffae1aca1f5640baa745
Daniel Cortes Pincheira <skrd159@gmail.com>
2016-09-21 12:02:40 -0300

    Primer avance, procesamiento de archivo de guardado

---
+++ general/Archivo.java
@@ -0,0 +1,55 @@
+package general;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Archivo {
+	private RandomAccessFile arch;
+	private String ruta;
+	public Archivo(String ruta){
+		this.ruta = ruta;
+	}
+	public RandomAccessFile getArch() {
+		return arch;
+	}
+	public void setArch(RandomAccessFile arch) {
+		this.arch = arch;
+	}
+	public String leerArchivo(){
+		String cadena;
+		String cadenaFinal = "";
+		try {
+			arch = new RandomAccessFile(ruta,"rw");
+		} catch (FileNotFoundException e) {
+			System.out.println("error al crear");
+		}
+		try {
+			while((cadena = arch.readLine())!=null)	{
+				cadenaFinal += cadena+"\n";
+			}
+			arch.close();
+		} catch (IOException e) {
+			System.out.println("error al leer");
+		}
+		return cadenaFinal;
+	}
+	public void escribirArchivo(String linea){
+		try {
+			arch = new RandomAccessFile(ruta,"rw");
+		} catch (FileNotFoundException e) {
+			System.out.println("error al crear");
+		}
+		try {
+			arch.setLength(0);
+			arch.writeBytes(linea);
+			arch.close();
+		} catch (Exception e) {
+			System.out.println("error al escribir");
+		} 
+	}
+	public String[] entregarProcesado(){
+		String[] datos = this.leerArchivo().split("\n");
+		return datos;
+	}
+}
